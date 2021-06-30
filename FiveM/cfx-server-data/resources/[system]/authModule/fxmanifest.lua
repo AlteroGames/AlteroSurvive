a9d8a558934c09afa82d1e3ad00bc6b33ce739bd
easyscripter <deniscs49@gmail.com>
2021-06-30 11:37:14 +0300

    feat(FiveM): create lua client script and manifest for authModule

---
+++ FiveM/cfx-server-data/resources/[system]/authModule/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games { 'gta5' }
+ui_page 'UI/index.html'
+files {
+    "UI/index.html",
+    "UI/libs/axios.min.js",
+    "UI/libs/vue.min.js",
+    "UI/index.js",
+    "UI/assets/*",
+    "UI/styles/*"
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua'
+}
