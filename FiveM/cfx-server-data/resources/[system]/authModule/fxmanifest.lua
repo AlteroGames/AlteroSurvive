02d37894dc60067d240e527e4508cb32a05214db
easyscripter <deniscs49@gmail.com>
2021-07-02 16:17:22 +0300

    style(FiveM): formatting code style in manifest

--- FiveM/cfx-server-data/resources/[system]/authModule/fxmanifest.lua
+++ FiveM/cfx-server-data/resources/[system]/authModule/fxmanifest.lua
@@ -1,19 +1,11 @@
 fx_version 'adamant'
-games { 'gta5' }
+games {'gta5'}
 ui_page 'UI/index.html'
 files {
-    "UI/index.html",
-    "UI/libs/axios.min.js",
-    "UI/libs/vue.min.js",
-    "UI/index.js",
-    "UI/assets/*",
-    "UI/styles/*"
+    "UI/index.html", "UI/libs/axios.min.js", "UI/libs/vue.min.js",
+    "UI/index.js", "UI/assets/*", "UI/styles/*"
 }
 
-client_scripts {
-    'client.lua',
-}
+client_scripts {'client.lua'}
 
-server_scripts {
-    'server.lua'
-}
+server_scripts {'server.lua'}
