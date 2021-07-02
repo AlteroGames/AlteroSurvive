e9234061a79d56e1533984c60ecf6d17e3b23aa1
easyscripter <deniscs49@gmail.com>
2021-07-02 16:16:06 +0300

    feat(FiveM): create client logic ui
    add showing ui and test chat messaging and freeze person

--- FiveM/cfx-server-data/resources/[system]/authModule/client.lua
+++ FiveM/cfx-server-data/resources/[system]/authModule/client.lua
@@ -1,1 +1,49 @@
+local uiEnabled = false
+
+function showUI(bool)
+    SetNuiFocus(bool, bool)
+    if bool == true then -- statement is only true if control is pressed
+        SendNUIMessage({showui = true}) -- send show ui = true to js
+    else
+        SendNUIMessage({showui = false})
+    end
+    return bool
+
+end
+
+function freezePerson(bool)
+    DisableControlAction(0, 1, bool)
+    DisableControlAction(0, 2, bool)
+    DisableControlAction(0, 142, bool)
+    DisableControlAction(0, 18, bool)
+    DisableControlAction(0, 322, bool)
+    DisableControlAction(0, 106, bool)
+end
+
+function chat(message, color)
+    TriggerEvent("chat:addMessage",
+                 {color = color, multiline = true, args = {message}})
+end
+
+RegisterNUICallback("get-data", function(data)
+    local login = data.login
+    local password = data.password
+
+    showUI(false)
+    freezePerson(false)
+    chat(string.format("Login is: %s Password is: %s", login, password),
+         {255, 255, 0})
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        AddEventHandler('onClientGameTypeStart', function()
+            uiEnabled = true
+            showUI(uiEnabled)
+            freezePerson(uiEnabled)
+        end)
+
+        Citizen.Wait(0)
+    end
+end)
 
